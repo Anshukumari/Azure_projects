994d8084733d318ef2a8b9c9b5c9a4b0816497ed
Anshu Kumari <anshukumari904@gmail.com>
2022-04-26 22:31:02 +0530

    DotNetCore Projects

---
+++ ActiveDirectoryWPFMsGraph/ActiveDirectoryWPFMsGraph/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ActiveDirectoryWPFMsGraph
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            _clientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .WithDefaultRedirectUri()
+                .Build();
+        }
+        private static string ClientId = "8d1fbc63-16e3-4e2f-8d17-c01a913eb660";
+        private static string Tenant = "common";
+
+        private static IPublicClientApplication _clientApp;
+
+        public static IPublicClientApplication PublicClientApp
+        {
+            get { return _clientApp; }
+        }
+    }
+}
